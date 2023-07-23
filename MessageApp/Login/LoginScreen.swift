b17acb787f97ad2fed964af77e22b87fe300fae2
Gabriel Mors <gabrielmors210@gmail.com>
2023-07-23 08:04:38 -0400

    set constraints end elements settins

--- MessageApp/Login/LoginScreen.swift
+++ MessageApp/Login/LoginScreen.swift
@@ -12,8 +12,9 @@ class LoginScreen: UIView {
     lazy var gmLogoImage: UIImageView = {
         let image = UIImageView()
         image.translatesAutoresizingMaskIntoConstraints = false
-        image.image = UIImage(named: "GM")
-        image.contentMode = .scaleAspectFit
+        image.image = UIImage(systemName: "message.badge.filled.fill")
+        image.tintColor = UIColor.darkGray
+        image.contentMode = .scaleAspectFill
         return image
     }()
     
@@ -22,9 +23,8 @@ class LoginScreen: UIView {
         label.translatesAutoresizingMaskIntoConstraints = false
         label.text = "E-mail"
         label.textColor = .black
-        label.textAlignment = .center
         label.numberOfLines = 0
-        label.font = UIFont.systemFont(ofSize: 18)
+        label.font = UIFont.systemFont(ofSize: 16)
         return label
     }()
     
@@ -36,6 +36,7 @@ class LoginScreen: UIView {
         textField.autocorrectionType = .no
         textField.spellCheckingType = .no
         textField.keyboardType = .default
+        textField.backgroundColor = .white
         return textField
     }()
     
@@ -44,9 +45,8 @@ class LoginScreen: UIView {
         label.translatesAutoresizingMaskIntoConstraints = false
         label.text = "Password"
         label.textColor = .black
-        label.textAlignment = .center
         label.numberOfLines = 0
-        label.font = UIFont.systemFont(ofSize: 18)
+        label.font = UIFont.systemFont(ofSize: 16)
         return label
     }()
     
@@ -58,12 +58,47 @@ class LoginScreen: UIView {
         textField.autocorrectionType = .no
         textField.spellCheckingType = .no
         textField.keyboardType = .default
+        textField.backgroundColor = .white
+        
         return textField
     }()
+    
+    lazy var EnterButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Log in", for: .normal)
+        button.setTitleColor(UIColor.white, for: .normal)
+        button.backgroundColor = UIColor.darkGray
+        button.clipsToBounds = true
+        button.layer.cornerRadius = 8
+        button.addTarget(self, action: #selector(tappedButton), for: .touchUpInside)
+        return button
+    }()
+    
+    @objc private func tappedButton() {
+        
+    }
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         addElements()
+        setConstraints()
+        addLeftPadding(to: emailTextField)
+        addLeftPadding(to: passwordTextField)
+        addBorder(to: emailTextField)
+        addBorder(to: passwordTextField)
+    }
+    
+    private func addLeftPadding(to textField: UITextField) {
+        let leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: textField.frame.height))
+        textField.leftView = leftView
+        textField.leftViewMode = .always
+    }
+    
+    private func addBorder(to: UITextField) {
+        to.layer.borderWidth = 1.5
+        to.layer.borderColor = UIColor.darkGray.cgColor
+        to.layer.cornerRadius = 15
     }
     
     private func addElements() {
@@ -72,6 +107,7 @@ class LoginScreen: UIView {
         addSubview(emailTextField)
         addSubview(passwordLabel)
         addSubview(passwordTextField)
+        addSubview(EnterButton)
     }
     
     required init?(coder: NSCoder) {
@@ -80,8 +116,33 @@ class LoginScreen: UIView {
     
     private func setConstraints() {
         NSLayoutConstraint.activate([
-     
+            gmLogoImage.centerXAnchor.constraint(equalTo: centerXAnchor),
+            gmLogoImage.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 20),
+            gmLogoImage.heightAnchor.constraint(equalToConstant: 120),
+            gmLogoImage.widthAnchor.constraint(equalToConstant: 120),
             
+            emailLabel.topAnchor.constraint(equalTo: gmLogoImage.bottomAnchor, constant: 40),
+            emailLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
+            emailLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
+
+            emailTextField.topAnchor.constraint(equalTo: emailLabel.bottomAnchor, constant: 2),
+            emailTextField.leadingAnchor.constraint(equalTo: emailLabel.leadingAnchor),
+            emailTextField.trailingAnchor.constraint(equalTo: emailLabel.trailingAnchor),
+            emailTextField.heightAnchor.constraint(equalToConstant: 40),
+            
+            passwordLabel.topAnchor.constraint(equalTo: emailTextField.bottomAnchor, constant: 20),
+            passwordLabel.leadingAnchor.constraint(equalTo: emailLabel.leadingAnchor),
+            passwordLabel.trailingAnchor.constraint(equalTo: emailLabel.trailingAnchor),
+            
+            passwordTextField.topAnchor.constraint(equalTo: passwordLabel.bottomAnchor, constant: 2),
+            passwordTextField.leadingAnchor.constraint(equalTo: emailLabel.leadingAnchor),
+            passwordTextField.trailingAnchor.constraint(equalTo: emailLabel.trailingAnchor),
+            passwordTextField.heightAnchor.constraint(equalToConstant: 40),
+            
+            EnterButton.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: 40),
+            EnterButton.leadingAnchor.constraint(equalTo: emailLabel.leadingAnchor),
+            EnterButton.trailingAnchor.constraint(equalTo: emailLabel.trailingAnchor),
+            EnterButton.heightAnchor.constraint(equalToConstant: 40)
         ])
     }
 }
--- MessageApp/Login/LoginViewController.swift
+++ MessageApp/Login/LoginViewController.swift
@@ -18,7 +18,7 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        view.backgroundColor = .lightGray
     }
 
 
