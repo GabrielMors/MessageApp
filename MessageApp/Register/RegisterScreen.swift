110d746a121e1e1ad98d21b6bd24ddbbaa4626dc
Gabriel Mors <gabrielmors210@gmail.com>
2023-07-23 21:25:08 -0400

    created register screen

---
+++ MessageApp/Register/RegisterScreen.swift
@@ -0,0 +1,19 @@
+//
+//  RegisterScreen.swift
+//  MessageApp
+//
+//  Created by Gabriel Mors  on 23/07/23.
+//
+
+import UIKit
+
+class RegisterScreen: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
